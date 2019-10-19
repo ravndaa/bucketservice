a42090d05e4c396b68ea0855a5e6dbc3a6c30fa0
Stian Ravndal <stian.ravndal@outlook.com>
2019-10-19 17:25:53 +0200

    adding testing.

---
+++ bucketservice_test.go
@@ -0,0 +1,33 @@
+package bucketservice
+
+import "testing"
+
+var bck = Init()
+
+func TestCreate(t *testing.T) {
+
+	expectedLength := 9
+	actual, err := bck.Create()
+
+	if len(actual) != expectedLength {
+		t.Errorf("Expected the Create to be %d but instead got %d!", expectedLength, len(actual))
+	} else if err != nil {
+		t.Errorf("Expcted error to be nil, but got: %s", err)
+	}
+
+}
+
+func TestDelete(t *testing.T) {
+
+	bucket, _ := bck.Create()
+	err := bck.Delete(bucket)
+
+	if err != nil {
+		t.Errorf("Expcted error to be nil, but got: %s", err)
+	}
+
+	if _, ok := bck.DB[bucket]; ok {
+		t.Errorf("Not expecting to find bucket.")
+	}
+
+}
