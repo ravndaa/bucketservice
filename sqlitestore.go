ec56e6052e5cec026b392cb8e876c04331af2e63
Stian Ravndal <stian.ravndal@outlook.com>
2019-10-20 22:44:34 +0200

    addedsql

--- sqlitestore.go
+++ sqlitestore.go
@@ -1,10 +1,36 @@
 package bucketservice
 
-import "github.com/jmoiron/sqlx"
+import (
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
+)
+
+/*
+Move to httpBucket when ready, but for now, here for testing in minibucket =)
+*/
 
 // SqliteStore how
 type SqliteStore struct {
 	Store *sqlx.DB
+}
+
+// InitDB a
+func (b *SqliteStore) InitDB() (sql.Result, error) {
+
+	schema := `
+		CREATE TABLE buckets (
+		id int,
+		bucketid text);
+		CREATE TABLE messages (
+			id int,
+			bucketid text
+			agentname text);`
+
+	// execute a query on the server
+	result, err := b.Store.Exec(schema)
+
+	return result, err
 }
 
 // Create a
@@ -26,16 +52,34 @@ func (b *SqliteStore) Delete() {
 }
 
 // AddMessage a
-func (b *SqliteStore) AddMessage() {
+func (b *SqliteStore) AddMessage(bucketid string, msg Message) error {
 
+	newBucket := `INSERT INTO messages (bucketid, agentname) VALUES (?, ?)`
+	_, err := b.Store.Exec(newBucket, bucketid, msg.AgentName)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // GetAllMessages a
-func (b *SqliteStore) GetAllMessages() {
-
+func (b *SqliteStore) GetAllMessages(bucketid string) (messages []Message, err error) {
+	err = b.Store.Select(&messages, "SELECT * FROM messages WHERE bucketid = ?", bucketid)
+	if err != nil {
+		return nil, err
+	}
+	return messages, nil
 }
 
 // DoesBucketExist a
-func (b *SqliteStore) DoesBucketExist() {
-
+func (b *SqliteStore) DoesBucketExist(bucketid string) bool {
+	var bucket Bucket
+	err := b.Store.Get(&bucket, "SELECT * FROM buckets WHERE bucketid = ?", bucketid)
+	if err != nil {
+		return false
+	}
+	if bucket.ID == "" {
+		return false
+	}
+	return true
 }
